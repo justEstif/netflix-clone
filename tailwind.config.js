305acb0cb136cc70089a7aaed5281b5f9b13d588
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-10 16:15:29 -0400

    added custom styles to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "netflix-background": 'url("/src/assets/netflixBackground.jpg")',
+      },
+      gridTemplateRows: {
+        // Complex site-specific row configuration
+        "navbar-content": "100px 1fr",
+      },
+      fontFamily: {
+        bebas: ["Bebas Neue", "cursive"],
+      },
+    },
   },
   plugins: [],
 };
