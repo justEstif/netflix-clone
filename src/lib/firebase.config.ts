c21e2511624e72bd48079173dcc3ec829aa58b12
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-10 06:30:45 -0400

    fixed typo in firebase config

--- src/lib/firebase.config.ts
+++ src/lib/firebase.config.ts
@@ -6,7 +6,7 @@ import { IFirebaseConfig } from "../helpers/Interfaces";
 const firebaseConfig: IFirebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_IDj,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
