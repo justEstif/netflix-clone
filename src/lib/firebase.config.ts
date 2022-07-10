9d6bf5c02613b78f9cdceda85da3622768375593
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-10 05:30:06 -0400

    created interface and fixed firebase config

--- src/lib/firebase.config.ts
+++ src/lib/firebase.config.ts
@@ -1,7 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { firebaseConfig } from "./endpoints.config";
+import { IFirebaseConfig } from "../helpers/Interfaces";
+
+const firebaseConfig: IFirebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_IDj,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
