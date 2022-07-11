cd5e69300332905ecaf0b038cc76069592ae67dc
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-11 15:47:15 -0400

    created a handleSubmit function for navigating to proper page

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,12 +1,29 @@
 import { useState, FunctionComponent } from "react";
 import { FaAngleRight } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import PageRoutes from "../../constants/Page.Routes";
+import { userEmailExistsInDB } from "../../services/firebase";
 
 interface LandingPageProps {}
 
+// ! if not navigate to the sign up page
 const LandingPage: FunctionComponent<LandingPageProps> = () => {
   const [userEmail, setUserEmail] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const checkUserEmail = async () => {
+      const userEmailExists = await userEmailExistsInDB(userEmail);
+      if (userEmailExists) {
+        navigate(PageRoutes.SignIn, { state: { userEmail } });
+      } else {
+        navigate(PageRoutes.SignUp, { state: { userEmail } });
+      }
+    };
+    checkUserEmail();
+  };
+
   return (
     <div className="h-full w-full bg-netflix-background bg-cover bg-center">
       <div className="top-0 left-0 fixed w-full h-screen bg-black/40">
@@ -16,41 +33,37 @@ const LandingPage: FunctionComponent<LandingPageProps> = () => {
               <Link to={PageRoutes.Landing}>Netflix</Link>
             </button>
             <div className="flex items-center gap-5 text-xl">
-              <button className="py-3 px-7 rounded-md hover:text-red-500">
-                <Link to={PageRoutes.SignIn}>Sign in</Link>
-              </button>
               <button className="bg-red-700 py-3 px-7 rounded-md hover:bg-red-800">
-                <Link to={PageRoutes.SignUp}>Sign up</Link>
+                <Link to={PageRoutes.SignIn}>Sign in</Link>
               </button>
             </div>
           </nav>
-          <section className="flex items-center justify-center">
-            <div className="flex flex-col gap-5 max-w-2xl text-center">
-              <div className="text-6xl font-extrabold">
-                Unlimited movies, TV shows, and more.
+          <section className="flex items-center justify-center relative">
+            <div className="text-center flex flex-col absolute top-20">
+              <div className="flex flex-col max-w-2xl gap-10 mb-10">
+                <p className="text-6xl font-extrabold">
+                  Unlimited movies, TV shows, and more.
+                </p>
+                <p className="text-4xl">Watch anywhere. Cancel anytime.</p>
+                <p className="text-3xl">
+                  Ready to watch? Enter your email to create or restart your
+                  membership.
+                </p>
               </div>
-              <div className="text-3xl">Watch anywhere. Cancel anytime.</div>
-              <div className="text-xl">
-                Ready to watch? Enter your email to create or restart your
-                membership.
-              </div>
-              <div className="flex justify-center items-center">
+              <form
+                onSubmit={(e) => handleSubmit(e)}
+                className="flex justify-center items-center">
                 <input
                   placeholder="Email address"
-                  type="text"
-                  className="py-7 text-black w-96 px-3 focus:outline-none"
+                  type="email"
+                  className="py-10 text-black flex-grow px-3 focus:outline-none duration-500 text-xl"
                   value={userEmail}
                   onChange={({ target }) => setUserEmail(target.value)}
                 />
-                <button className="text-xl rounded-md bg-red-700 px-10 py-7">
-                  <Link
-                    to={PageRoutes.SignUp}
-                    state={userEmail}
-                    className="flex items-center justify-center ">
-                    Get Started <FaAngleRight />
-                  </Link>
+                <button type="submit" className="text-xl bg-red-700 p-10 flex">
+                  <p>Get Started</p> <FaAngleRight />
                 </button>
-              </div>
+              </form>
             </div>
           </section>
         </div>
