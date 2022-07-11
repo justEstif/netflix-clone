2e36e4a896ac0ef79f6cb67f95f55454e414bdec
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-11 15:48:25 -0400

    included the signin page to the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { FunctionComponent, Fragment, lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PageRoutes from "./constants/Page.Routes";
+
 interface AppProps {}
 
 const LandingPage = lazy(() => import("./pages/LandingPage/LandingPage"));
 const SignUpPage = lazy(() => import("./pages/SignUpPage/SignUpPage"));
+const SignInPage = lazy(() => import("./pages/SignInPage/SignInPage"));
 
 const App: FunctionComponent<AppProps> = () => {
   return (
@@ -13,6 +15,7 @@ const App: FunctionComponent<AppProps> = () => {
         <Routes>
           <Route path={PageRoutes.Landing} element={<LandingPage />} />
           <Route path={PageRoutes.SignUp} element={<SignUpPage />} />
+          <Route path={PageRoutes.SignIn} element={<SignInPage />} />
         </Routes>
       </Suspense>
     </Router>
