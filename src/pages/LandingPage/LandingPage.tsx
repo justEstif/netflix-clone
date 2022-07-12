9c46332caafa0dc5a355d493425d5936f13f840b
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-12 11:35:29 -0400

    simple formatting for easier vim nav

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -28,44 +28,57 @@ const LandingPage: FunctionComponent<LandingPageProps> = () => {
     <div className="h-full w-full bg-netflix-background bg-cover bg-center">
       <div className="top-0 left-0 fixed w-full h-screen bg-black/40">
         <div className="h-full w-full grid grid-rows-navbar-content text-white container mx-auto p-4">
+
           <nav className="flex justify-between">
             <button className=" shadow-2xl text-red-700 text-5xl uppercase tracking-wider font-bebas">
               <Link to={PageRoutes.Landing}>Netflix</Link>
             </button>
+
             <div className="flex items-center gap-5 text-xl">
               <button className="bg-red-700 py-3 px-7 rounded-md hover:bg-red-800">
-                <Link to={PageRoutes.SignIn}>Sign in</Link>
+                <Link to={PageRoutes.SignIn} state={{ userEmail: null }}>
+                  Sign in
+                </Link>
               </button>
             </div>
           </nav>
+
           <section className="flex items-center justify-center relative">
             <div className="text-center flex flex-col absolute top-20">
               <div className="flex flex-col max-w-2xl gap-10 mb-10">
                 <p className="text-6xl font-extrabold">
                   Unlimited movies, TV shows, and more.
                 </p>
+
                 <p className="text-4xl">Watch anywhere. Cancel anytime.</p>
                 <p className="text-3xl">
                   Ready to watch? Enter your email to create or restart your
                   membership.
                 </p>
               </div>
+
               <form
                 onSubmit={(e) => handleSubmit(e)}
                 className="flex justify-center items-center">
+
                 <input
                   placeholder="Email address"
                   type="email"
+                  required
                   className="py-10 text-black flex-grow px-3 focus:outline-none duration-500 text-xl"
                   value={userEmail}
                   onChange={({ target }) => setUserEmail(target.value)}
                 />
+
                 <button type="submit" className="text-xl bg-red-700 p-10 flex">
                   <p>Get Started</p> <FaAngleRight />
                 </button>
+
               </form>
+
             </div>
           </section>
+
         </div>
       </div>
     </div>
--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -1,12 +1,18 @@
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent, useEffect, useRef, useState } from "react";
 import { useLocation, Link } from "react-router-dom";
 import PageRoutes from "../../constants/Page.Routes";
-interface SignInPageProps {}
+interface SignInPageProps { }
 
 interface CustomizedState {
   userEmail: string;
 }
+
 const SignInPage: FunctionComponent<SignInPageProps> = () => {
+  const [userEmail, setUserEmail] = useState(""); // email state
+  const { userEmail: userEmailFromInput } = useLocation()
+    .state as CustomizedState;
+  const userEmailInputRef = useRef<HTMLInputElement>(null);
+  const passwordInputRef = useRef<HTMLInputElement>(null);
 
   const handleSubmit = (e:React.FormEvent) => {
     e.preventDefault()
@@ -32,11 +38,13 @@ const SignInPage: FunctionComponent<SignInPageProps> = () => {
     <div className="h-full w-full bg-netflix-background bg-cover bg-center">
       <div className="top-0 left-0 fixed w-full h-screen bg-black/40">
         <div className="h-full w-full grid grid-rows-navbar-content text-white container mx-auto p-4">
+
           <nav className="flex justify-between">
             <button className=" shadow-2xl text-red-700 text-5xl uppercase tracking-wider font-bebas">
               <Link to={PageRoutes.Landing}>Netflix</Link>
             </button>
           </nav>
+
           <section className="flex items-center justify-center relative ">
             <div className="bg-black/70 absolute top-10 px-16 py-10 w-full max-w-md">
 
@@ -64,25 +72,30 @@ const SignInPage: FunctionComponent<SignInPageProps> = () => {
                   placeholder="Password"
                   className="px-5 py-4 rounded bg-gray-400/40 text-gray-200 focus:outline-none focus:border-b-4 border-orange-500 duration-300"
                 />
+
                 <button
                   type="submit"
                   className="rounded-md text-xl py-2 bg-red-600 w-full text-white hover:bg-red-700">
                   Sign In
                 </button>
+
                 <div className="flex justify-between items-center text-gray-400">
                   <div className="flex justify-between items-center gap-2">
                     <input type="checkbox" className="" />
                     <p>Remember Me</p>
                   </div>
-                  <p className="">Need Help?</p>
+                  <p>Need Help?</p>
                 </div>
+
               </form>
+
               <div className="py-4">
                 <p className="text-gray-400">
                   New to Netflix?{" "}
                   <span className="text-white">Sign up now.</span>
                 </p>
               </div>
+
             </div>
           </section>
         </div>
