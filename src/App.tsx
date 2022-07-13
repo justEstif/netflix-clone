0994a056675e42d0966545b40cd0945ee8696b44
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-13 13:50:07 -0400

    passing the user from the useAuthListener hook to all routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import { FunctionComponent, Fragment, lazy, Suspense } from "react";
+import { FunctionComponent, Fragment, lazy, Suspense, useContext } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PageRoutes from "./constants/Page.Routes";
 
 interface AppProps {}
+import UserContext from "./context/UserContext"
+import useAuthListener from "./hooks/useAuthListener"
 
 const LandingPage = lazy(() => import("./pages/LandingPage/LandingPage"));
 const SignUpPage = lazy(() => import("./pages/SignUpPage/SignUpPage"));
 const SignInPage = lazy(() => import("./pages/SignInPage/SignInPage"));
 
 const App: FunctionComponent<AppProps> = () => {
+  const { user } = useAuthListener();
   return (
-    <Router>
-      <Suspense fallback={<Fragment></Fragment>}>
-        <Routes>
-          <Route path={PageRoutes.Landing} element={<LandingPage />} />
-          <Route path={PageRoutes.SignUp} element={<SignUpPage />} />
-          <Route path={PageRoutes.SignIn} element={<SignInPage />} />
-        </Routes>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<Fragment></Fragment>}>
+          <Routes>
+            <Route path={PageRoutes.Landing} element={<LandingPage />} />
+            <Route path={PageRoutes.SignUp} element={<SignUpPage />} />
+            <Route path={PageRoutes.SignIn} element={<SignInPage />} />
+            <Route path={PageRoutes.Home} element={<HomePage />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 };
 
