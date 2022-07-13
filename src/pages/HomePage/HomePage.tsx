da0fd658cab9031e729d88e609881d64fc605b30
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-13 15:59:22 -0400

    made changed to homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,28 +2,15 @@ import { FunctionComponent, useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import PageRoutes from "../../constants/Page.Routes";
 import FirebaseContext from "../../context/FirebaseContext";
-import { requests, getMoviesFromUrl } from "../../services/tmdb"
 // import UserContext from "../../context/UserContext";
 import { signOut, Auth } from "firebase/auth";
+import Hero from "../../components/Hero"
 
 interface HomeProps { }
 
-const randomEl = (arr: []) => arr[Math.floor(Math.random() * arr.length)];
-const truncateString = (str: string) => `${str?.split(" ", 20).join(" ")} ...`;
 const Home: FunctionComponent<HomeProps> = () => {
   const firebase = useContext(FirebaseContext);
   const auth = firebase?.auth as Auth;
-  const [movies, setMovies] = useState([]);
-  const [movie, setMovie] = useState();
-
-  useEffect(() => {
-    const getMovies = async () => {
-      const popularMovies = await getMoviesFromUrl(requests.requestPopular);
-      setMovies(popularMovies);
-      setMovie(randomEl(popularMovies));
-    }
-    getMovies()
-  }, []);
 
   return (
     <div className="h-full w-full bg-black">
@@ -47,7 +34,9 @@ const Home: FunctionComponent<HomeProps> = () => {
             </button>
           </div>
         </nav>
-        <main className="bg-yellow-200">Image here</main>
+
+<Hero />
+
         <section className="bg-red-300">
           <div><button>Click THis</button></div>
           <div>Row</div>
