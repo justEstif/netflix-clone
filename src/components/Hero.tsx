c4e8639c11c48ee4ae735175274e1081f23459ce
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-14 04:40:02 -0400

    minor styling to Hero components

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,9 +12,10 @@ type Movie = {
 };
 
 const Hero = () => {
+
   const [movies, setMovies] = useState<Movie[]>([]);
   const [movie, setMovie] = useState<Movie>();
-  //
+
   const truncateString = (str: string | undefined) => {
     if (typeof str === "string") {
       return `${str?.split(" ", 30).join(" ")} ...`;
@@ -22,6 +23,7 @@ const Hero = () => {
   };
 
   const randomEl = (arr: Movie[]): Movie => arr[Math.floor(Math.random() * arr.length)];
+
   const getGenre = (movie: Movie | undefined) => {
     const currentMovieGenres = movie?.genre_ids
     if (currentMovieGenres) {
@@ -33,6 +35,7 @@ const Hero = () => {
         }
         return result
       }, [])
+
       return genresWords.join(", ")
     }
   };
@@ -46,7 +49,7 @@ const Hero = () => {
     getMovies();
   }, []);
 
-  useEffect(() => {
+  useEffect(() => { // slideshow
     const interval = setInterval(() => {
       setMovie(randomEl(movies));
     }, 30000);
@@ -54,24 +57,24 @@ const Hero = () => {
   });
 
   return (
-    <div
+    <main
       style={{
         backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`,
       }}
-      className="h-full w-full grid grid-rows-navbar-hero bg-cover bg-no-repeat">
+      className="h-full w-full grid grid-rows-navbar-hero bg-cover bg-no-repeat bg-gradient-to-tr from-black">
       <UserNavBar />
-      <section className="w-full h-full bg-cover bg-top flex items-end container mx-auto p-4">
+      <section className="w-full h-full bg-cover bg-top flex items-end container mx-auto p-4 drop-shadow-lg">
         <div className="p-4 max-w-3xl w-full h-4/6 flex flex-col gap-4">
           <h1 className="text-3xl md:text-6xl font-bold text-white">
             {movie?.title}
           </h1>
-          <div className="text-white text-xl">{getGenre(movie)}</div>
-          <div className="text-white text-xl">Released: {movie?.release_date}</div>
-          <div className="w-full text-white text-xl">{truncateString(movie?.overview)}</div>
-          <div className="w-full text-white text-xl">Rating: {movie?.vote_average}</div>
+          <p className="text-white text-xl">{getGenre(movie)}</p>
+          <p className="text-white text-xl">Released: {movie?.release_date}</p>
+          <p className="w-full text-white text-xl">Rating: {movie?.vote_average}</p>
+          <p className="w-full text-white text-lg">{truncateString(movie?.overview)}</p>
         </div>
       </section>
-    </div>
+    </main>
   );
 };
 export default Hero;
