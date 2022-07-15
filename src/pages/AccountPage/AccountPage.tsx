633c0b6d1e137e03ba5189d514b1cbb2730f3848
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-15 15:10:43 -0400

    getting the liked movies and passing them to account row

--- src/pages/AccountPage/AccountPage.tsx
+++ src/pages/AccountPage/AccountPage.tsx
@@ -1,37 +1,37 @@
-import React, { FunctionComponent, useContext, useEffect, useState } from "react";
+import React, {
+  FunctionComponent,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import AccountRow from "../../components/AccountRow";
 import { requests, getMovieInfo } from "../../services/tmdb";
-import UserNavBar from "../../components/UserNavBar"
+import UserNavBar from "../../components/UserNavBar";
 import UserContext from "../../context/UserContext";
+import IMovie from "../../interfaces/IMovie";
 
 const AccountPage: FunctionComponent = () => {
-  const user = useContext(UserContext)
-  const [movies, setMovies] = useState(user?.user?.userDoc?.liked)
-  const [title, setTitle] = useState("")
+  const user = useContext(UserContext);
+  const likedMovies = user?.user?.userDoc?.liked;
+  const [movies, setMovies] = useState<IMovie[] | null>();
 
+  // page loads
   useEffect(() => {
-    const getLikedMoviesInfo = async () => {
-      const allMovies = []
-      const likedMovies = user?.user?.userDoc?.liked
-      for (const likedMovie of likedMovies) {
-        const movieInDb = await getMovieInfo(likedMovie)
-        allMovies.push(movieInDb)
-      }
-      setMovies(() => allMovies)
-    }
-    getLikedMoviesInfo()
-  }, [])
-
+    const getArrayOfMovies = async (likedMovies: number[]) => {
+      const unresolvedPromises = likedMovies.map((likedMovie) =>
+        getMovieInfo(likedMovie)
+      );
+      const allMovies = await Promise.all(unresolvedPromises);
+      setMovies(() => allMovies);
+    };
+    likedMovies && getArrayOfMovies(likedMovies);
+  }, [likedMovies]);
 
   return (
     <div className="h-full w-full flex flex-col bg-black">
       <UserNavBar />
       <section className="w-full mx-auto px-4 flex flex-col gap-7">
-        {
-          movies && (
-            <AccountRow title={"Liked"} movies={movies} />
-          )
-        }
+        {movies && <AccountRow title={"Liked"} movies={movies} />}
       </section>
     </div>
   );
