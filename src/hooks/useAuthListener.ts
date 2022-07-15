428ba5fb93be6617a6033c68d3330a8a8488c0ee
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-15 15:09:59 -0400

    adding the user doc id and items to the user context

--- src/hooks/useAuthListener.ts
+++ src/hooks/useAuthListener.ts
@@ -1,7 +1,8 @@
 // ! hook that listens to auth change
 import { useState, useEffect, useContext } from "react";
 import FirebaseContext from "../context/FirebaseContext";
-import { Auth, onAuthStateChanged } from "firebase/auth";
+import { User, Auth, onAuthStateChanged } from "firebase/auth";
+import { getUserDoc } from "../services/firebase";
 
 const useAuthListener = () => {
   const [user, setUser] = useState(
@@ -22,6 +23,21 @@ const useAuthListener = () => {
     });
     return () => listener();
   }, [auth]);
+
+  useEffect(() => {
+    const getUserDocFromDb = async (userEmail: string) => {
+      const userDoc = await getUserDoc(userEmail);
+      setUser((prevState: User) => {
+        return {
+          ...prevState,
+          userDoc,
+        };
+      });
+    };
+    // only called if there is a user
+    if (user?.email) getUserDocFromDb(user?.email);
+  }, [auth]);
+
   return { user };
 };
 
