9ddf145e5f8c42a58b913bad5e2bcf8be47b15fe
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-15 03:26:19 -0400

    extracted movie type

---
+++ src/interfaces/IMovie.ts
@@ -0,0 +1,9 @@
+export default interface IMovie {
+  title?: string;
+  backdrop_path?: string;
+  release_date?: string;
+  overview?: string;
+  vote_average?: string;
+  genre_ids?: number[];
+  poster_path?: string;
+}
