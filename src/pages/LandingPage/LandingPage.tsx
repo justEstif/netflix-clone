67fbba5bbc90ebf668a1ce178269bd7dac51c84c
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-15 03:24:38 -0400

    fixed background not showing up

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,14 +1,12 @@
-import { useState, FunctionComponent } from "react";
+import React, { useState, FunctionComponent } from "react";
 import { FaAngleRight } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import PageRoutes from "../../constants/Page.Routes";
 import { userEmailExistsInDB } from "../../services/firebase";
-import LandingNavBar from "../../components/LandingNavBar"
-
-interface LandingPageProps { }
+import LandingNavBar from "../../components/LandingNavBar";
 
 // ! if not navigate to the sign up page
-const LandingPage: FunctionComponent<LandingPageProps> = () => {
+const LandingPage: FunctionComponent = () => {
   const [userEmail, setUserEmail] = useState("");
   const navigate = useNavigate();
 
@@ -26,48 +24,41 @@ const LandingPage: FunctionComponent<LandingPageProps> = () => {
   };
 
   return (
-    <div className="h-full w-full bg-netflix-background bg-cover bg-center">
-      <div className="top-0 left-0 fixed w-full h-screen bg-black/40">
-        <div className="h-full w-full grid grid-rows-navbar-content text-white container mx-auto p-4">
+    <div className="top-0 left-0 fixed w-full h-full bg-netflix-background bg-cover bg-center">
+      <div className="h-full w-full grid grid-rows-navbar-content text-white container mx-auto p-4  ">
+        <LandingNavBar />
+        <section className="flex items-center justify-center relative">
+          <div className="text-center flex flex-col absolute top-20">
+            <div className="flex flex-col max-w-2xl gap-10 mb-10">
+              <p className="text-6xl font-extrabold">
+                Unlimited movies, TV shows, and more.
+              </p>
 
-          <LandingNavBar />
-          <section className="flex items-center justify-center relative">
-            <div className="text-center flex flex-col absolute top-20">
-              <div className="flex flex-col max-w-2xl gap-10 mb-10">
-                <p className="text-6xl font-extrabold">
-                  Unlimited movies, TV shows, and more.
-                </p>
-
-                <p className="text-4xl">Watch anywhere. Cancel anytime.</p>
-                <p className="text-3xl">
-                  Ready to watch? Enter your email to create or restart your
-                  membership.
-                </p>
-              </div>
-
-              <form
-                onSubmit={(e) => handleSubmit(e)}
-                className="flex justify-center items-center">
-
-                <input
-                  placeholder="Email address"
-                  type="email"
-                  required
-                  className="py-10 text-black flex-grow px-3 focus:outline-none duration-500 text-xl"
-                  value={userEmail}
-                  onChange={({ target }) => setUserEmail(target.value)}
-                />
-
-                <button type="submit" className="text-xl bg-red-700 p-10 flex">
-                  <p>Get Started</p> <FaAngleRight />
-                </button>
-
-              </form>
-
+              <p className="text-4xl">Watch anywhere. Cancel anytime.</p>
+              <p className="text-3xl">
+                Ready to watch? Enter your email to create or restart your
+                membership.
+              </p>
             </div>
-          </section>
 
-        </div>
+            <form
+              onSubmit={(e) => handleSubmit(e)}
+              className="flex justify-center items-center">
+              <input
+                placeholder="Email address"
+                type="email"
+                required
+                className="py-10 text-black flex-grow px-3 focus:outline-none duration-500 text-xl"
+                value={userEmail}
+                onChange={({ target }) => setUserEmail(target.value)}
+              />
+
+              <button type="submit" className="text-xl bg-red-700 p-10 flex">
+                <p>Get Started</p> <FaAngleRight />
+              </button>
+            </form>
+          </div>
+        </section>
       </div>
     </div>
   );
--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useEffect, useRef, useState } from "react";
+import React, { FunctionComponent, useEffect, useRef, useState } from "react";
 import { useLocation, Link, useNavigate } from "react-router-dom";
 import PageRoutes from "../../constants/Page.Routes";
 import { signUpUser } from "../../services/firebase";
@@ -16,7 +16,7 @@ const SignUpPage: FunctionComponent<ISignUpPage> = () => {
 
   const { userEmail: userEmailFromInput } = useLocation()
     .state as CustomizedState;
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const userEmailInputRef = useRef<HTMLInputElement>(null);
   const passwordInputRef = useRef<HTMLInputElement>(null);
@@ -37,81 +37,76 @@ const SignUpPage: FunctionComponent<ISignUpPage> = () => {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    setError("")
+    setError("");
     const handleSignUp = async () => {
-      const { response, message } = await signUpUser(userEmail, password)
+      const { response, message } = await signUpUser(userEmail, password);
       if (response) {
-        navigate(PageRoutes.Home)
+        navigate(PageRoutes.Home);
       } else {
         setError(message);
       }
-    }
-    handleSignUp()
-  }
+    };
+    handleSignUp();
+  };
 
   return (
-    <div className="h-full w-full bg-netflix-background bg-cover bg-center">
-      <div className="top-0 left-0 fixed w-full h-screen bg-black/40">
-        <div className="h-full w-full grid grid-rows-navbar-content text-white container mx-auto p-4">
-          <nav className="flex justify-between">
-            <button className=" text-red-700 text-6xl uppercase tracking-wider font-bebas">
-              <Link to={PageRoutes.Landing}>Netflix</Link>
-            </button>
-          </nav>
+    <div className="top-0 left-0 fixed w-full h-full bg-netflix-background bg-cover bg-center">
+      <div className="h-full w-full grid grid-rows-navbar-content text-white container mx-auto p-4  ">
+        <nav className="flex justify-between">
+          <button className=" text-red-700 text-6xl uppercase tracking-wider font-bebas">
+            <Link to={PageRoutes.Landing}>Netflix</Link>
+          </button>
+        </nav>
 
-          <section className="flex items-center justify-center relative ">
-            <div className="bg-black/70 absolute top-10 px-16 py-10 w-full max-w-md">
-              <form
-                onSubmit={(e) => handleSubmit(e)}
-                className="flex flex-col gap-10 w-full mb-3">
-                <h1 className="text-white text-3xl font-semibold tracking-wider">
-                  Sign Up
-                </h1>
-                {error && <p>{error}</p>}
-                <input
-                  value={userEmail}
-                  required
-                  ref={userEmailInputRef}
-                  onChange={({ target }) => setUserEmail(target.value)}
-                  type="email"
-                  placeholder="Email Address"
-                  className="px-5 py-4 rounded bg-gray-400/40 text-gray-200 focus:outline-none focus:border-b-4 border-orange-500 duration-300"
-                />
-                <input
-                  required
-                  ref={passwordInputRef}
-                  value={password}
-                  onChange={({ target }) => setPassword(target.value)}
-                  type="password"
-                  placeholder="Password"
-                  className="px-5 py-4 rounded bg-gray-400/40 text-gray-200 focus:outline-none focus:border-b-4 border-orange-500 duration-300"
-                />
-                <button
-                  type="submit"
-                  className="rounded-md text-xl py-2 bg-red-600 w-full text-white hover:bg-red-700">
-                  Sign Up
-                </button>
-              </form>
+        <section className="flex items-center justify-center relative ">
+          <div className="bg-black/70 absolute top-10 px-16 py-10 w-full max-w-md">
+            <form
+              onSubmit={(e) => handleSubmit(e)}
+              className="flex flex-col gap-10 w-full mb-3">
+              <h1 className="text-white text-3xl font-semibold tracking-wider">
+                Sign Up
+              </h1>
+              {error && <p>{error}</p>}
+              <input
+                value={userEmail}
+                required
+                ref={userEmailInputRef}
+                onChange={({ target }) => setUserEmail(target.value)}
+                type="email"
+                placeholder="Email Address"
+                className="px-5 py-4 rounded bg-gray-400/40 text-gray-200 focus:outline-none focus:border-b-4 border-orange-500 duration-300"
+              />
+              <input
+                required
+                ref={passwordInputRef}
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+                type="password"
+                placeholder="Password"
+                className="px-5 py-4 rounded bg-gray-400/40 text-gray-200 focus:outline-none focus:border-b-4 border-orange-500 duration-300"
+              />
+              <button
+                type="submit"
+                className="rounded-md text-xl py-2 bg-red-600 w-full text-white hover:bg-red-700">
+                Sign Up
+              </button>
+            </form>
 
-              <div className="py-4">
-                <p className="text-gray-400">
-                  Already have an account?{" "}
-                  <span className="text-white">
-                    <Link to={PageRoutes.SignIn} state={{ userEmail: null }}>
-                      Sign in
-                    </Link>
-                  </span>
-                </p>
-              </div>
+            <div className="py-4">
+              <p className="text-gray-400">
+                Already have an account?{" "}
+                <span className="text-white">
+                  <Link to={PageRoutes.SignIn} state={{ userEmail: null }}>
+                    Sign in
+                  </Link>
+                </span>
+              </p>
             </div>
-          </section>
-        </div>
+          </div>
+        </section>
       </div>
     </div>
   );
 };
 
 export default SignUpPage;
-
-
-
