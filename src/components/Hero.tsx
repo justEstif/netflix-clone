a2da56465fe2b2c887d11902d20faa6430c4b01f
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-15 03:25:49 -0400

    made improvements to the hero compoent

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,11 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import UserNavBar from "./UserNavBar";
 import { requests, getMoviesFromUrl, movieGenres } from "../services/tmdb";
-
-type Movie = {
-  title?: string;
-  backdrop_path?: string;
-  release_date?: string;
-  overview?: string;
-  vote_average?: string;
-  genre_ids?: number[];
-};
+import IMovie from "../interfaces/IMovie";
 
 const Hero = () => {
-
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [movie, setMovie] = useState<Movie>();
+  const [movies, setMovies] = useState<IMovie[]>([]);
+  const [movie, setMovie] = useState<IMovie>();
 
   const truncateString = (str: string | undefined) => {
     if (typeof str === "string") {
@@ -22,21 +13,21 @@ const Hero = () => {
     }
   };
 
-  const randomEl = (arr: Movie[]): Movie => arr[Math.floor(Math.random() * arr.length)];
+  const randomEl = (arr: IMovie[]): IMovie =>
+    arr[Math.floor(Math.random() * arr.length)];
 
-  const getGenre = (movie: Movie | undefined) => {
-    const currentMovieGenres = movie?.genre_ids
+  const getGenre = (movie: IMovie | undefined) => {
+    const currentMovieGenres = movie?.genre_ids;
     if (currentMovieGenres) {
       const genresWords = movieGenres.reduce((result: string[], el) => {
-
         if (currentMovieGenres.includes(el.id)) {
-          const movieGenre: string = el.genre!
-          result.push(movieGenre)
+          const movieGenre: string = el.genre!;
+          result.push(movieGenre);
         }
-        return result
-      }, [])
+        return result;
+      }, []);
 
-      return genresWords.join(", ")
+      return genresWords.join(", ");
     }
   };
 
@@ -49,10 +40,11 @@ const Hero = () => {
     getMovies();
   }, []);
 
-  useEffect(() => { // slideshow
+  useEffect(() => {
+    // slideshow
     const interval = setInterval(() => {
       setMovie(randomEl(movies));
-    }, 30000);
+    }, 40000);
     return () => clearInterval(interval);
   });
 
@@ -61,7 +53,7 @@ const Hero = () => {
       style={{
         backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`,
       }}
-      className="h-full w-full grid grid-rows-navbar-hero bg-cover bg-no-repeat bg-gradient-to-tr from-black">
+      className="w-full grid grid-rows-navbar-hero bg-cover bg-no-repeat bg-gradient-to-tr from-black duration-200">
       <UserNavBar />
       <section className="w-full h-full bg-cover bg-top flex items-end container mx-auto p-4 drop-shadow-lg">
         <div className="p-4 max-w-3xl w-full h-4/6 flex flex-col gap-4">
@@ -70,8 +62,12 @@ const Hero = () => {
           </h1>
           <p className="text-white text-xl">{getGenre(movie)}</p>
           <p className="text-white text-xl">Released: {movie?.release_date}</p>
-          <p className="w-full text-white text-xl">Rating: {movie?.vote_average}</p>
-          <p className="w-full text-white text-lg">{truncateString(movie?.overview)}</p>
+          <p className="w-full text-white text-xl">
+            Rating: {movie?.vote_average}
+          </p>
+          <p className="w-full text-white text-lg">
+            {truncateString(movie?.overview)}
+          </p>
         </div>
       </section>
     </main>
